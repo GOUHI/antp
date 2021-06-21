06ca8b8e51a18fe69a1ab24114040ca9f92c90a1
王华杰 <755913611@qq.com>
2021-06-21 09:11:26 +0800

    假数据菜单

--- admin/src/components/layout/Home.vue
+++ admin/src/components/layout/Home.vue
@@ -18,60 +18,108 @@
       </a-menu>
     </a-layout-sider> -->
     <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
-       <div class="logo" ><h3>logo</h3></div>
-        <a-menu
-          mode="inline"
-          :default-selected-keys="['1']"
-          :default-open-keys="['sub1']"
-          :style="{ borderRight: 0}"
-        >
-          <a-sub-menu key="sub1">
-            <span slot="title"><a-icon type="user" />{{collapsed ? '' : 'subnav 1' }}</span>
-            <a-menu-item key="1">
-              option1
+      <div class="logo" >
+         <h3>logo</h3>
+      </div>
+      <a-menu
+        mode="inline"
+        :default-selected-keys="['1']"
+        :default-open-keys="['sub1']"
+        :style="{ borderRight: 0}"
+      >
+        <template v-for="(firstItem) in menuData">
+          <template v-if="firstItem.subMenus && firstItem.subMenus.length > 0">
+            <a-sub-menu :key="firstItem.id">
+              <span slot="title"><a-icon :type="firstItem.icon" />{{collapsed ? '' : firstItem.name }}</span>
+              <template v-if="firstItem.subMenus">
+                <template v-for="secondItem in firstItem.subMenus">
+                  <template v-if="secondItem.subMenus && secondItem.subMenus.length > 0">
+                    <a-sub-menu :key="secondItem.id" :title="secondItem.name">
+                      <!-- <span slot="title"><a-icon :type="secondItem.icon" />{{collapsed ? '' : secondItem.name }}</span> -->
+                      <template v-for="thirdItem in secondItem.subMenus">
+                        <a-menu-item v-if="!thirdItem.hide" :key="thirdItem.id">
+                          {{thirdItem.name}}
+                        </a-menu-item>
+                      </template>
+                    </a-sub-menu>
+                  </template>
+                  <template v-else>
+                    <a-menu-item v-if="!secondItem.hide" :key="secondItem.id">
+                      {{ secondItem.name }}
+                    </a-menu-item>
+                  </template>
+                </template>
+              </template>
+              <template v-else>
+                <a-menu-item>
+                  {{ firstItem.name }}
+                </a-menu-item>
+              </template>
+            </a-sub-menu>
+          </template>
+
+          <template v-else>
+            <a-menu-item :key="firstItem.id">
+              <a-icon :type="firstItem.icon" />
+              <span>{{firstItem.name}}</span>
             </a-menu-item>
-            <a-menu-item key="2">
-              option2
-            </a-menu-item>
-            <a-menu-item key="3">
-              option3
-            </a-menu-item>
-            <a-menu-item key="4">
-              option4
-            </a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title"><a-icon type="laptop" />{{collapsed ? '' : 'subnav 2' }}</span>
-            <a-menu-item key="5">
-              option5
-            </a-menu-item>
-            <a-menu-item key="6">
-              option6
-            </a-menu-item>
+          </template>
+        </template>
+
+        <!-- <a-sub-menu key="sub1">
+          <span slot="title"><a-icon type="user" />{{collapsed ? '' : '用户管理' }}</span>
+          <a-menu-item key="1">
+            用户列表
+          </a-menu-item>
+          <a-menu-item key="2">
+            用户统计
+          </a-menu-item>
+          <a-menu-item key="3">
+            登录记录
+          </a-menu-item>
+          <a-menu-item key="4">
+            option4
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title"><a-icon type="laptop" />{{collapsed ? '' : 'subnav 2' }}</span>
+          <a-menu-item key="5">
+            option5
+          </a-menu-item>
+          <a-menu-item key="6">
+            option6
+          </a-menu-item>
+          <a-menu-item key="7">
+            option7
+          </a-menu-item>
+          <a-menu-item key="8">
+            option8
+          </a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <span slot="title"><a-icon type="notification" />{{collapsed ? '' : 'subnav 3' }}</span>
+          <a-menu-item key="9">
+            option9
+          </a-menu-item>
+          <a-menu-item key="10">
+            option10
+          </a-menu-item>
+          <a-menu-item key="11">
+            option11
+          </a-menu-item>
+          <a-sub-menu key="000" title="Submenu">
             <a-menu-item key="7">
-              option7
+              Option 7
             </a-menu-item>
             <a-menu-item key="8">
-              option8
+              Option 8
             </a-menu-item>
           </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <span slot="title"><a-icon type="notification" />{{collapsed ? '' : 'subnav 3' }}</span>
-            <a-menu-item key="9">
-              option9
-            </a-menu-item>
-            <a-menu-item key="10">
-              option10
-            </a-menu-item>
-            <a-menu-item key="11">
-              option11
-            </a-menu-item>
-            <a-menu-item key="12">
-              option12
-            </a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
+        </a-sub-menu> -->
+
+
+      </a-menu>
+    </a-layout-sider>
     <a-layout>
       <a-layout-header style="background: #fff; padding: 0">
         <a-icon
@@ -95,6 +143,34 @@ export default {
   data() {
     return {
       collapsed: false,
+      menuData:[
+        { id:1,
+          name:"用户管理",
+          icon:'user',
+          subMenus:[
+            {id:2,name:'用户列表',hide:false},
+            {id:3,name:'用户统计',hide:false,subMenus:[
+              {id:4,name:'行为统计',hide:false},
+              {id:5,name:'营销统计',hide:false},
+              {id:5,name:'3333',hide:true},
+            ]},
+            {id:6,name:'登录记录',hide:false}
+          ]
+        },
+        { id:7,
+          name:"系统管理",
+          icon:'laptop',
+          subMenus:[
+            {id:8,name:'操作日志',hide:false},
+            {id:9,name:'修复密码',hide:false},
+            {id:10,name:'ssss',hide:true}
+          ]
+        },
+        { id:11,
+          name:"控制台",
+          icon:'laptop'
+        },
+      ]
     };
   },
 }
